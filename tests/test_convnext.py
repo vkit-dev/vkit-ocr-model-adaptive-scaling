373d13c746bb8186e69a9f4e2862e27b370805af
Hunt Zhan <huntzhan.dev@gmail.com>
2022-06-05 23:36:49 +0800

    test_convnext_jit

--- tests/test_convnext.py
+++ tests/test_convnext.py
@@ -1,4 +1,5 @@
 import torch
+import iolite as io
 from vkit_open_model.model.convnext import ConvNext
 
 
@@ -16,7 +17,6 @@ def print_convnext_tiny():
 
 def test_convnext():
     model = ConvNext.create_tiny()
-    model = torch.jit.script(model)  # type: ignore
     x = torch.rand((1, 3, 320, 320))
     features = model(x)  # type: ignore
     assert len(features) == 4
@@ -24,3 +24,29 @@ def test_convnext():
     assert features[1].shape == (1, 192, 40, 40)
     assert features[2].shape == (1, 384, 20, 20)
     assert features[3].shape == (1, 768, 10, 10)
+
+
+def test_convnext_jit():
+    model = ConvNext.create_tiny()
+    model_jit = torch.jit.script(model)  # type: ignore
+
+    x = torch.rand((1, 3, 320, 320))
+    features = model_jit(x)  # type: ignore
+    assert len(features) == 4
+    assert features[0].shape == (1, 96, 80, 80)
+    assert features[1].shape == (1, 192, 40, 40)
+    assert features[2].shape == (1, 384, 20, 20)
+    assert features[3].shape == (1, 768, 10, 10)
+
+    out_fd = io.folder(
+        '$VKIT_OPEN_MODEL_DATA/test_convnext',
+        expandvars=True,
+        touch=True,
+    )
+    torch.save(
+        {'model': model_jit.state_dict()},  # type: ignore
+        out_fd / 'torch-save-convnext-state-dict.pt',
+    )
+
+    dump = torch.load(out_fd / 'torch-save-convnext-state-dict.pt')
+    model.load_state_dict(dump['model'])
